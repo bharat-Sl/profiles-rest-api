35897f6dfe99e3571bc126b6de155dd4ba1f4c3e
bharat-Sl <bharatag00@gmail.com>
2024-12-17 17:15:14 +0530

    added hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView # type: ignore
+from rest_framework.response import Response # type: ignore
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API VIEW"""
+
+    def get(self,request,format = None):
+        """Returns a lst of alphabets"""
+        li = ['a','b','c','d']
+
+        return Response({'message':'Success!','list':li})
